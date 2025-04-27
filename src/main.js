d547fedd0f026a9c9500271f06e707634960906f
Mykhailo <hryvinskyim@gmail.com>
2025-04-27 15:55:19 +0300

    added mobile menu

--- src/main.js
+++ src/main.js
@@ -4,4 +4,31 @@ import { onThemeChanges } from './JS/theemes';
 
 const themeContainer = document.querySelector('.container-theems');
 
+const menu = document.querySelector('.js-menu');
+const btnMenu = document.querySelector('.js-menu-toggle');
+const anchorLinks = document.querySelectorAll('.header__navigation-link');
+
+const menuToggle = () => {
+    menu.classList.toggle('open-menu');
+    btnMenu.classList.toggle('icon-chenges');
+
+   menu.classList.contains('open-menu') 
+   ? document.body.style.overflow = "hidden" 
+   : document.body.style.overflow = "";
+}
+
+const menuClose = () => {
+    menu.classList.remove('open-menu');
+    document.body.style.overflow = '';
+}
+
+anchorLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        if (menu.classList.contains('open-menu')) {
+            menuClose();
+        }
+    });
+});
+
+btnMenu.addEventListener('click', menuToggle);
 themeContainer.addEventListener('change', onThemeChanges);
